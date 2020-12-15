bcf50e36bc7eebfab3ed469690b9b14b5b32756b
Mujib <mujib517@gmail.com>
2020-12-15 09:01:02 +0530

    Add Math file

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -8,6 +8,7 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        int res = new MyMath().add(10, 20);
+        System.out.println(res);
     }
 }
