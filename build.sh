4c89a246ef4c61ca0b84923e6038766096f2cf26
md.rahman <md.rahman@thoughtworks.com>
2020-12-16 09:21:22 +0530

    Add manifest file

--- build.sh
+++ build.sh
@@ -1,3 +1,9 @@
-cd out/production/devops-demo-1
+cd out/production/devops-java
 
 jar cmvf META-INF/MANIFEST.mf app.jar *.class
+
+mv app.jar ../../../
+
+// java website
+// deploy multiple server
+// load balancer
